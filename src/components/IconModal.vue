8bbe416ca45da9ae1922dba92be27840de9383b8
Tony Myers <tony@myers.co.nz>
2023-05-02 20:26:38 +1200

    Adding syntax highlighting and copy code button

--- src/components/IconModal.vue
+++ src/components/IconModal.vue
@@ -4,20 +4,32 @@
   >
     <div class="bg-white p-4 rounded-lg shadow-lg mx-auto max-w-6xl">
       <pre
-        class="bg-slate-800 p-4 text-white"
-      ><code class="whitespace-pre-wrap">{{ svgCode }}</code></pre>
+        class="bg-slate-800 text-white"
+      ><code class="whitespace-pre-wrap"><highlightjs id="icon-code" autodetect :code="svgCode" /></code></pre>
       <button
         class="mt-4 bg-gray-500 hover:bg-gray-600 text-white py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600"
         @click="$emit('close')"
       >
         Close
       </button>
+      <button
+        @click="copyCode"
+        class="mt-4 ml-2 text-white py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-emerald-600"
+        :class="{
+          'bg-emerald-500 hover:bg-emerald-600': !copied,
+          'bg-gray-500 hover:bg-gray-500 text-gray-200': copied,
+        }"
+        :disabled="copied"
+      >
+        <span v-if="!copied">Copy code</span>
+        <span v-if="copied">Code copied..</span>
+      </button>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   name: "IconModal",
@@ -26,6 +38,25 @@ export default defineComponent({
       type: String,
       required: true,
     },
+  },
+  methods: {
+    copyCode() {
+      const code = document.querySelector("#icon-code")?.textContent;
+      if (code) {
+        navigator.clipboard.writeText(code);
+        this.copied = true;
+        setTimeout(() => {
+          this.copied = false;
+        }, 2000);
+      }
+    },
+  },
+  setup() {
+    const copied = ref(false);
+
+    return {
+      copied,
+    };
   },
 });
 </script>
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,15 @@ import { createApp } from "vue";
 import "./assets/tailwind.css";
 import App from "./App.vue";
 import VueSvgInlinePlugin from "vue-svg-inline-plugin";
+import "highlight.js/styles/stackoverflow-dark.css";
+import hljs from "highlight.js/lib/core";
+import javascript from "highlight.js/lib/languages/javascript";
+import hljsVuePlugin from "@highlightjs/vue-plugin";
+
+hljs.registerLanguage("javascript", javascript);
 
 const app = createApp(App);
+app.use(hljsVuePlugin);
 
 app.use(VueSvgInlinePlugin, {
   attributes: {
